a2e85a387b5cc512fab078120516ea277f52943a
wuge <wuge@tuya.com>
2018-09-14 16:00:06 +0800

    init

---
+++ NSData+CRC32.h
@@ -0,0 +1,15 @@
+//
+//  NSData+CRC32.h
+//  CRC32_iOS
+//
+//  Created by 吴戈 on 2018/9/14.
+//  Copyright © 2018年 wuge. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSData (CRC32)
+
+-(int32_t) crc32;
+
+@end
